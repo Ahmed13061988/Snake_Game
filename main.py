4bb61ebec8da9c83067008b4f9c3b6fdbdca6c0a
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-02 12:44:03 -0600

    adding snake body

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from turtle import Screen, Turtle
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-
-
-
-
-
-
+for position in starting_position:
+    new_snake = Turtle("square")
+    new_snake.color("white")
+    new_snake.goto(position)
 
 screen.exitonclick()
