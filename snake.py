4277582696a178ac0b5c841815f77caef86d6d90
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-02 15:01:18 -0600

    Controlling the snake ( up, sown, right, left) and preventing the snake to go the opposite direction

--- snake.py
+++ snake.py
@@ -2,12 +2,17 @@ from turtle import Turtle
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.snakes = []
         self.create_sneak()
+        self.head = self.snakes[0]
 
     def create_sneak(self):
         for position in STARTING_POSITION:
@@ -22,16 +27,20 @@ class Snake:
             new_x = self.snakes[seg_num - 1].xcor()
             new_y = self.snakes[seg_num - 1].ycor()
             self.snakes[seg_num].goto(new_x, new_y)
-        self.snakes[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.snakes[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.snakes[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.snakes[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.snakes[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
