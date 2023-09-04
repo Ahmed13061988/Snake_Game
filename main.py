5a93c909aa8bada4068616c741ebcc87e574e52b
Ahmed Hussein <wolktheline@yahoo.com>
2023-09-03 21:47:17 -0600

    Using slicing to use only one if statement

--- main.py
+++ main.py
@@ -31,9 +31,8 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score.game_over()
-    for i in snake.snakes:
-        if i != snake.head:
-            if snake.head.distance(i) < 10:
-                game_is_on = False
-                score.game_over()
+    for i in snake.snakes[1:]:
+        if snake.head.distance(i) < 10:
+            game_is_on = False
+            score.game_over()
 screen.exitonclick()
